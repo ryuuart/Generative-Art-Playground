f5c2df4c4a0dcea17c4f6fa93df0ace503020a30
Long Nguyen <18nguyenl@gmail.com>
2021-02-27 14:33:43 -0600

    Set up web structure

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        port: 9000,
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    }
+}
